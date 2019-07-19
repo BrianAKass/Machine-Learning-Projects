13253c70735460db618829ee329004ddb5586d39
village-ID10T <33644215+village-ID10T@users.noreply.github.com>
2019-07-19 12:42:08 -0400

    ML notes learning R Studio

---
+++ PML Projects/RStudio files/Intro.R
@@ -0,0 +1,17 @@
+### vectors, data , matrices, subsetting
+# comment out 
+x=c(2,7,5)
+x
+y=seq(from=4,length=3,by=3)
+y
+x+y
+x/y
+x^y
+x[2]
+
+x[1]
+x[2:3]
+x[-2]
+x[-1]
+x[-c(1,2)]
+z=matrix(seq(1,12),4,3)
