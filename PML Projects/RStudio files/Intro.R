b1b6b466a424c77d2ec5bf4a369369f5ec1e8069
BrianAKass <kasinator009@gmail.com>
2019-07-22 16:45:01 -0400

    commit after file migration day after crash

--- PML Projects/RStudio files/Intro.R
+++ PML Projects/RStudio files/Intro.R
@@ -15,3 +15,31 @@ x[-2]
 x[-1]
 x[-c(1,2)]
 z=matrix(seq(1,12),4,3)
+z
+z[3:4,2:3]
+z[,2:3]
+z[,1]
+z[4,1]
+z
+z[,1,drop=FALSE]
+dim(z)
+ls()
+rm(y)
+ls()
+x= runif(50)
+y=rnorm(50)
+y
+plot(x,y)
+plot(x,y,xlab="Random uniform",ylab="Random normal", col="blue" )
+par(mfrow=c(1,1))
+plot(x,y)
+Auto=read.csv('filelocationandname')
+names(Auto)#self explanitory
+dim(Auto)#dimension of data
+class(Auto)#lets us know things like series or dataframe
+summary(Auto)#shows the min max mean etc of each variable
+plot(Auto$cylinders,Auto$mpg)
+attach(Auto) #puts all the variables in your workspace
+search() # tells us our various workspaces
+plot(cylinders,mpg)
+cylinders-as.factor(cylinders)
